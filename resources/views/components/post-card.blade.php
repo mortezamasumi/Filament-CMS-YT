f1d6509cf045209319fb49e48ef3da3f6708f322
Morteza Masumi <morteza.masumi@gmail.com>
2023-12-30 13:15:35 +0330

    some

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -1,29 +1,28 @@
-<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+<div class="dark:bg-gray-800 dark:border-gray-700 max-w-sm bg-white border border-gray-200 rounded-lg shadow">
     <a href="{{ route('post.show', $post) }}" wire:navigate>
-        <img class="rounded-t-lg" src="{{ $post->getFirstMediaUrl() }}" alt="{{ $post->title }}"/>
+        <img class="rounded-t-lg" src="{{ $post->getFirstMediaUrl() }}" alt="{{ $post->title }}" />
     </a>
     <div class="p-5">
         <header class="lg:mb-6 not-format">
             <div class="mb-4">
-                @foreach($post->categories as $category)
+                @foreach ($post->categories as $category)
                     <a href="{{ route('category.show', $category->slug) }}" wire:navigate>
-                <span style="background-color:{{ $category->bg_color }};color: {{ $category->text_color }};"
-                      class="text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
-                    {{ $category->title }}
-                </span>
+                        <span style="background-color:{{ $category->bg_color }};color: {{ $category->text_color }};"
+                            class="text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
+                            {{ $category->title }}
+                        </span>
                     </a>
                 @endforeach
             </div>
             <address class="flex items-center mb-6 not-italic">
-                <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
-                    <img class="mr-4 w-8 h-8 rounded-full" src="{{ $post->user->profile_photo_url }}"
-                         alt="Jese Leos">
+                <div class="dark:text-white inline-flex items-center mr-3 text-sm text-gray-900">
+                    <img class="w-8 h-8 mr-4 rounded-full" src="{{ $post->user->profile_photo_url }}" alt="Jese Leos">
                     <div>
                         <a href="#" rel="author"
-                           class="font-bold text-gray-900 dark:text-white">{{ $post->user->name }}</a>
-                        <p class="text-base text-gray-500 dark:text-gray-400">
+                            class="dark:text-white font-bold text-gray-900">{{ $post->user->name }}</a>
+                        <p class="dark:text-gray-400 text-base text-gray-500">
                             <time datetime="{{ $post->created_now }}"
-                                  title="{{ \Carbon\Carbon::parse($post->created_at)->format('F jS, Y') }}">
+                                title="{{ \Carbon\Carbon::parse($post->created_at)->format('F jS, Y') }}">
                                 {{ \Carbon\Carbon::parse($post->created_at)->format('F jS, Y') }}
                             </time>
                         </p>
@@ -32,18 +31,18 @@
             </address>
         </header>
         <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $post->title }}</h5>
+            <h5 class="dark:text-white mb-2 text-2xl font-bold tracking-tight text-gray-900">{{ $post->title }}</h5>
         </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        <p class="dark:text-gray-400 mb-3 font-normal text-gray-700">
             {!! $post->excerpt() !!}
         </p>
         <a href="{{ route('post.show', $post) }}" wire:navigate
-           class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            class="hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg">
             Read more
             <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                 viewBox="0 0 14 10">
+                viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                    d="M1 5h12m0 0L9 1m4 4L9 9" />
             </svg>
         </a>
     </div>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,42 +1,42 @@
 <x-app-layout>
     @section('title', $post->title)
     @section('meta_description', $post->meta_description)
-    <main class="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900 antialiased">
-        <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
+    <main class="lg:pt-16 lg:pb-24 dark:bg-gray-900 pt-8 pb-16 antialiased bg-white">
+        <div class=" flex justify-between max-w-screen-xl px-4 mx-auto">
             <article
-                class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+                class="format format-sm sm:format-base lg:format-lg format-blue dark:format-invert w-full max-w-2xl mx-auto">
                 <div class="mb-5">
-                    <img src="{{ $post->getFirstMediaUrl() }}" class="w-full"/>
+                    <img src="{{ $post->getFirstMediaUrl() }}" class="w-full" />
                 </div>
                 <div class="mb-5">
-                    @foreach($post->categories as $category)
+                    @foreach ($post->categories as $category)
                         <a href="{{ route('category.show', $category->slug) }}" wire:navigate>
-                            <span
-                                style="background-color:{{ $category->bg_color }};color: {{ $category->text_color }};"
+                            <span style="background-color:{{ $category->bg_color }};color: {{ $category->text_color }};"
                                 class="text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
                                 {{ $category->title }}
                             </span>
                         </a>
                     @endforeach
                 </div>
-                <header class="mb-4 lg:mb-6 not-format">
+                <header class="lg:mb-6 not-format mb-4">
                     <address class="flex items-center mb-6 not-italic">
-                        <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
-                            <img class="mr-4 w-16 h-16 rounded-full" src="{{ $post->user->profile_photo_url }}"
-                                 alt="Jese Leos">
+                        <div class="dark:text-white inline-flex items-center mr-3 text-sm text-gray-900">
+                            <img class="w-16 h-16 mr-4 rounded-full" src="{{ $post->user->profile_photo_url }}"
+                                alt="Jese Leos">
                             <div>
                                 <a href="#" rel="author"
-                                   class="text-xl font-bold text-gray-900 dark:text-white">{{ $post->user->name }}</a>
-                                <p class="text-base text-gray-500 dark:text-gray-400">
+                                    class="dark:text-white text-xl font-bold text-gray-900">{{ $post->user->name }}</a>
+                                <p class="dark:text-gray-400 text-base text-gray-500">
                                     <time datetime="{{ $post->created_now }}"
-                                          title="{{ \Carbon\Carbon::parse($post->created_at)->format('F jS, Y') }}">
+                                        title="{{ \Carbon\Carbon::parse($post->created_at)->format('F jS, Y') }}">
                                         {{ \Carbon\Carbon::parse($post->created_at)->format('F jS, Y') }}
                                     </time>
                                 </p>
                             </div>
                         </div>
                     </address>
-                    <h1 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">
+                    <h1
+                        class="lg:mb-6 lg:text-4xl dark:text-white mb-4 text-3xl font-extrabold leading-tight text-gray-900">
                         {{ $post->title }}
                     </h1>
                 </header>
